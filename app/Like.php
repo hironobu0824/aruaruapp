7a26543410e945527a2c2bfb5df3cb40ccc0decf
hironobu0824 <banu.asika0824.nobu@gmail.com>
2020-12-24 15:10:49 +0900

    修正

--- app/Like.php
+++ app/Like.php
@@ -8,7 +8,7 @@ class Like extends Model
 {
     protected $fillable = ['post_id','user_id'];
     
-    public function reply()
+    public function post()
     {
        return $this->belongsTo(Post::class);
     }
