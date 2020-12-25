d5a013bfe056caca53af906eeb1acbef3cf82cfc
hironobu0824 <banu.asika0824.nobu@gmail.com>
2020-12-25 15:54:24 +0900

    カテゴリ修正

--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -14,72 +14,22 @@ class CategoryTableSeeder extends Seeder
         DB::table('categories')->truncate();
         DB::table('categories')->insert([
             [
-                'name' => 'イベント、季節',
+                'name' => '学生向け',
                 'updated_at' => now(),
                 'created_at' => now(),
             ],  
             [
-                'name' => 'スポーツ、アウトドア',
+                'name' => '社会人向け',
                 'updated_at' => now(),
                 'created_at' => now(),
             ],  
             [
-                'name' => 'SNS、アプリ、テレビ',
+                'name' => '全般',
                 'updated_at' => now(),
                 'created_at' => now(),
             ],  
             [
-                'name' => '健康、美容、ファッション',
-                'updated_at' => now(),
-                'created_at' => now(),
-            ],  
-            [
-                'name' => '恋愛',
-                'updated_at' => now(),
-                'created_at' => now(),
-            ],  
-            [
-                'name' => '学校、教育、学問',
-                'updated_at' => now(),
-                'created_at' => now(),
-            ],  
-            [
-                'name' => '音楽、ゲーム',
-                'updated_at' => now(),
-                'created_at' => now(),
-            ],  
-            [
-                'name' => 'マンガ、映画、本',
-                'updated_at' => now(),
-                'created_at' => now(),
-            ],  
-            [
-                'name' => '食事、料理',
-                'updated_at' => now(),
-                'created_at' => now(),
-            ],  
-            [
-                'name' => '暮らし、住まい、家事',
-                'updated_at' => now(),
-                'created_at' => now(),
-            ],  
-            [
-                'name' => '趣味全般',
-                'updated_at' => now(),
-                'created_at' => now(),
-            ],  
-            [
-                'name' => 'ショッピング、お金',
-                'updated_at' => now(),
-                'created_at' => now(),
-            ],  
-            [
-                'name' => '旅行、地域、交通',
-                'updated_at' => now(),
-                'created_at' => now(),
-            ],  
-            [
-                'name' => 'その他',
+                'name' => 'マニアック',
                 'updated_at' => now(),
                 'created_at' => now(),
             ],  
